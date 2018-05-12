6a71099fcc86dd7be27bcfbea89f09033e26a575
LEEYAEJI <33189409+YAEJILEE@users.noreply.github.com>
2018-05-12 10:17:58 +0000

    Update MainActivity.java

--- LAB331/app/src/main/java/com/example/xnote/lab331/MainActivity.java
+++ LAB331/app/src/main/java/com/example/xnote/lab331/MainActivity.java
@@ -1,52 +1,113 @@
-package com.example.xnote.lab331;
+
+
+package com.ch.lab3;
+
+ 
 
 import android.graphics.Color;
+
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.view.ContextMenu;
+
+import
+android.support.v7.app.AppCompatActivity;
+
+import
+android.view.ContextMenu;
+
 import android.view.MenuItem;
+
 import android.view.View;
+
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
-    Button mBtn;
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+ 
 
-        mBtn = (Button)findViewById(R.id.btn);
+public class MainActivity
+extends AppCompatActivity {
 
-        // Register Button For Context Menu.
-        registerForContextMenu(mBtn);
-    }
+    Button mBtn;
 
-    @Override
-    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
-        super.onCreateContextMenu(menu,v,menuInfo);
+    @Override
 
-        // Set Menu's Title.
-        menu.setHeaderTitle("Button Menu");
+    protected void onCreate(Bundle
+savedInstanceState) {
 
-        // Inflate Menu layout for Context Menu.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-    }
+        super.onCreate(savedInstanceState);
 
-    @Override
-    public boolean onContextItemSelected(MenuItem item) {
-        // Checking menu's ID and change button's text color.
-        switch(item.getItemId()) {
-            case R.id.red:
-                mBtn.setTextColor(Color.RED);
-                break;
-            case R.id.green:
-                mBtn.setTextColor(Color.GREEN);
-                break;
-            case R.id.blue:
-                mBtn.setTextColor(Color.BLUE);
-                break;
-        }
+        setContentView(R.layout.activity_main);
 
-        return super.onContextItemSelected(item);
-    }
+ 
+
+        mBtn = (Button)findViewById(R.id.btn);
+
+
+        registerForContextMenu(mBtn);
+
+    }
+
+ 
+
+    @Override
+
+    public void onCreateContextMenu(ContextMenu
+menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+
+       
+super.onCreateContextMenu(menu,v,menuInfo);
+
+ 
+
+        // Set Title.
+
+        menu.setHeaderTitle("Button
+Menu");
+
+ 
+
+        // Inflate Menu layout
+
+       
+getMenuInflater().inflate(R.menu.menu_main, menu);
+
+    }
+
+ 
+
+    @Override
+
+    public boolean
+onContextItemSelected(MenuItem item) {
+
+        // Checking menu's ID and change button's text color.
+
+        switch(item.getItemId()) {
+
+            case R.id.red:
+
+                mBtn.setTextColor(Color.RED);
+
+                break;
+
+            case R.id.green:
+
+                mBtn.setTextColor(Color.GREEN);
+
+                break;
+
+            case R.id.blue:
+
+                mBtn.setTextColor(Color.BLUE);
+
+                break;
+
+        }
+
+ 
+
+        return
+super.onContextItemSelected(item);
+
+    }
+
 }
+
